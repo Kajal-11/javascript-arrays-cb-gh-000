2d57ecb1b28c19bd3aa6725623a8469f2f4be713
Kajal Goel <kajal11122000@gmail.com>
2019-06-15 09:54:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars= ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element){
   var array1 = array;
   array1.unshift(element);
-  return array1;
+  return array;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
