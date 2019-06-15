9ee5677d6f1ff221646929c7a13a7112516ee71e
Kajal Goel <kajal11122000@gmail.com>
2019-06-15 09:49:40 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars= ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  var array1 = array.unshift(element);
+  return array1
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element);
+}
+
+function addElementToEndOfArray(array, element){
+  var array1 = array.push(element);
+  return array1
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  return array.push(element);
+}
