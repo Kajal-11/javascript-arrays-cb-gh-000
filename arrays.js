d7e4becca4c7e0aed79d668829cef89e4aa48bef
Kajal Goel <kajal11122000@gmail.com>
2019-06-15 09:59:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,3 +21,17 @@ function destructivelyAddElementToEndOfArray(array, element){
   array.push(element);
   return array;
 }
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  var array1 = array.splice(1);
+  return array1;
+}
